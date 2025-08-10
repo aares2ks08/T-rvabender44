e34b91b6d489a993bdacaabfa0e1d86d9826c52a
aares2ks08 <aares2ks@gmail.com>
2025-08-10 21:16:39 +0300

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Serve static files from 'public' folder (optional, if you have other assets)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve your custom HTML on root or custom route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public-fourdigit-password.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
