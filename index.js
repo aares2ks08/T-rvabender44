a3019af7fcf4a019152018ceca055356e36f8bde
aares2ks08 <aares2ks@gmail.com>
2025-08-10 20:43:19 +0300

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-const correctPassword = "2008";
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Serve static files (like HTML, CSS)
+// Serve static files from 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Handle login POST request
-app.post('/login', (req, res) => {
-  const { password } = req.body;
-  if (password === correctPassword) {
-    res.send('<h2 style="color:green;">Access Granted!</h2>');
-  } else {
-    res.send('<h2 style="color:red;">Incorrect Password. Try again.</h2><a href="/">Back</a>');
-  }
-});
-
-// Start the server
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
